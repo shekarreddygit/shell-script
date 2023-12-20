cb6cd87dad62f4f0b32d89e183459eeb53d046dc
shekarreddygit <shekarreddy0301@gmail.com>
2023-12-21 00:44:51 +0530

    third commit

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!bin/shell
+
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1 : Hi $PERSON2, good morning"
+echo "$PERSON2 : Hi $PERSON1, very good morning"
+echo "$PERSON1 : How are you ?"
+echo "$PERSON2 : I am fine, how are you?"
